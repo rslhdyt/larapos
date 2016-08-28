8afc507a0e2c66e6e8e2dd7d9c62781d4c73618e
Risal Hidayat <risalhidayat88@gmail.com>
2016-08-28 09:19:21 +0700

    membuat seeder untuk table users

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
