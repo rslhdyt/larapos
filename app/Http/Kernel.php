6d370c24b86cf006600149c0e03c78ba9d23771e
Risal Hidayat <risalhidayat88@gmail.com>
2016-10-21 10:18:56 +0700

    passport: add token to csrf

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'api' => [
