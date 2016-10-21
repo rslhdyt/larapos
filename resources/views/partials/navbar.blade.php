f321d2df0b0d5fdf523e814e659e80c4c6817961
Risal Hidayat <risalhidayat88@gmail.com>
2016-10-21 20:20:14 +0700

    update root url settings

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -57,7 +57,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('settings') }}">Settings</a></li>
+                            <li><a href="{{ url('settings/profile') }}">Settings</a></li>
                             <li role="separator" class="divider"></li>
                             <li>
                                 <a href="{{ url('/logout') }}"
