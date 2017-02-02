b098634bef8adc79af704dfdfb07098d25d68ccc
Risal Hidayat <risalhidayat88@gmail.com>
2017-02-02 19:20:28 +0700

    add softdelete feature in some master data

--- app/Customer.php
+++ app/Customer.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
+     use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
+    ];
+
     /**
      * rules validasi untuk data customers.
      *
