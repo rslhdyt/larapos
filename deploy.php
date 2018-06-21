878496583535ea67e68433eb4f243a9e14bc0a70
Risal Hidayat <risalhidayat88@gmail.com>
2018-06-21 14:54:03 +0700

    initial commit new larapos

---
+++ deploy.php
@@ -0,0 +1,46 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'larapos');
+
+// Project repository
+set('repository', 'git@github.com:rslhdyt/larapos.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+// Hosts
+host('sandbox.web.id')
+    ->user('app')
+    ->forwardAgent()
+    ->stage('sandbox')
+    ->set('deploy_path', '~/htdocs/{{application}}');
+    
+// Tasks
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo /etc/init.d/php7.2-fpm restart');
+});
+after('deploy:symlink', 'php-fpm:restart');
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
